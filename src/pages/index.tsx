eec55986a18db3ba2913de2c009ffff1e37bfadc
giovani-viana <giovaniviana2011@gmail.com>
2024-03-08 11:46:36 -0300

    First

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+    return (
+        <main>
+            <section >
+                <h1>Cortes</h1>
+            </section>
+            <section>
+                <h1>Nossos Proficionais</h1>
+            </section>
+            <section>
+                <h1>Serviços</h1>
+            </section>
+            <section>
+                <h1>Fotos</h1>
+            </section>
+        </main>
+    )
+}
