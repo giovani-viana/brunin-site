1a05b079f5aa474a5d26ea5da822bc14de5b6524
giovani-viana <giovaniviana2011@gmail.com>
2024-03-12 14:48:52 -0300

    Página Contato

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -3,45 +3,52 @@ import styles from "./styles.module.css";
 
 const contato: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <>
       <p className={styles.title}>Contato</p>
+      <div className={styles.container}>
+        <div className={styles.map_Container}>
+          <div className={styles.text_Container}>
+            <p className={styles.Title_info}>VENHA NOS VISITAR</p>
+            <p className={styles.Title_info}>Taguatinga, rua 23</p>
+            <p className={styles.Title_info}>EMAIL: gasjdij </p>
+            <p className={styles.Title_info}>TEL: (11) 9999-9999</p>
+            <p className={styles.Title_info}>Segunda a Sexta:</p>
+            <p className={styles.Title_info}>Sabado:</p>
+          </div>
 
-      <div className={styles.map_Container}>
-        <div className={styles.text_Container}>
-          <p className={styles.Title_info}>VENHA NOS VISITAR</p>
-          <p className={styles.Title_info}>Taguatinga, rua 23</p>
-          <p className={styles.Title_info}>EMAIL: gasjdij </p>
-          <p className={styles.Title_info}>TEL: (11) 9999-9999</p>
-          <p className={styles.Title_info}>Segunda a Sexta:</p>
-          <p className={styles.Title_info}>Sabado:</p>
+          <form className={styles.form}>
+            <p className={styles.title}>Mande uma mensagem</p>
+
+            <input
+              className={styles.field1}
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Nome"
+            />
+
+            <input
+              className={styles.field2}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="E-mail"
+            />
+            <textarea
+              className={styles.field3}
+              id="message"
+              name="message"
+              rows={4}
+              placeholder="Digite sua mensagem"
+            />
+
+            <button type="submit" className={styles.button}>
+              Enviar
+            </button>
+          </form>
         </div>
-
-        <form className={styles.form}>
-          <p>Mande uma mensagem</p>
-          <label className={styles.label} htmlFor="name">
-            Name:
-          </label>
-          <input className={styles.input} type="text" id="name" name="name" />
-
-          <label className={styles.label} htmlFor="email">
-            Email:
-          </label>
-          <input
-            className={styles.input}
-            type="email"
-            id="email"
-            name="email"
-          />
-
-          <label className={styles.label} htmlFor="message">
-            Message:
-          </label>
-          <textarea id="message" name="message" rows={4} />
-
-          <button type="submit">Submit</button>
-        </form>
       </div>
-    </div>
+    </>
   );
 };
 
